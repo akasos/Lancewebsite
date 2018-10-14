8026c0100fc36a2450e2cd592a512a6c77ce59d7
Austin Stephens <austinmoptop@gmail.com>
2018-10-14 13:48:10 -0600

    first commit

---
+++ script/script.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function()
+{   
+      $(function(){
+        $(".header-nav-welcome").load("header-nav-welcome.html");
+    });
+
+    $(function(){
+        $(".footer").load("footer.html");
+    });
+
+    
+});
+
